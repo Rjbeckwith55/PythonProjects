fa10432c4500dde34f930251e04c4e861f27b390
Robert Beckwith <rjbeckwith55@gmail.com>
2017-05-25 20:29:08 -0500

    commit

--- PythonApplication1/keyboard_handler.py
+++ PythonApplication1/keyboard_handler.py
@@ -1,6 +1,6 @@
 import pygame
 import time
-#screen = pygame.display.set_mode((600,400))
+
 
 CLOCK = pygame.time.Clock()
 clock_speed = 20
@@ -11,8 +11,14 @@ pygame.init()
 print ('initialized')
 
 
+pygame.init()
+
+screen = pygame.display.set_mode((600,400))
+
+
 while running:
     keys = pygame.key.get_pressed()
+    pygame.display.update()
 
     if keys[pygame.K_LEFT]:
         positions[0] += 5  # Increase the positon of the base by 5 degrees
@@ -22,7 +28,6 @@ while running:
         positions[0] -= 5  # Decrease the positon of the base by 5 degrees
         #BP.set_motor_position(PORT_A,postions[0])
         print ("Base postion: ", positions[0])
-
     if keys[pygame.K_UP]:
         positions[1] += 5
         #BP.set_motor_position(PORT_B,postions[1])
@@ -33,3 +38,4 @@ while running:
         print ("Arm position: ", positions[1])
     if keys[pygame.K_1]:
         print ('1')
+    CLOCK.tick(clock_speed)
