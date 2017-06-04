5573ca6f5723ec0cf15f805f359783f082fed0f3
Robert Beckwith <rjbeckwith55@gmail.com>
2017-06-04 01:56:38 -0500

    Added files

---
+++ PythonApplication1/game.py
@@ -0,0 +1,22 @@
+import pygame
+import sys
+import time
+from ship import Ship
+
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((640,480))
+    pygame.display.set_caption("Game Title")
+    
+    #Make a new ship
+    ship = Ship(screen)
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        ship.blitme()
+        pygame.display.flip()
+run_game()
